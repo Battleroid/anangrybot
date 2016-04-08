20d39c22ab5471ef096b0053f73b0fe50e328728
Casey Weed <me@caseyweed.net>
2016-04-07 23:57:04 -0400

    rewrite of markov, some issues, but seems to produce more fitting results

--- markov.py
+++ markov.py
@@ -1,64 +1,87 @@
-from itertools import izip
-from random import choice
+import random
+import re
 
 
 class Markov(object):
 
-    EOS = ('.', '!', '?')
+    def __init__(self, filename):
+        self.words = self.file_to_words(filename)
+        self.starters = []
+        self.temp_db = {}
+        self.db = {}
+        self.build_db(self.words)
 
-    def __init__(self, training_file, size=3):
-        self.training_file = training_file
-        self.lines = self.file_to_lines(self.training_file)
-        self.database = dict()
-        self._build_database(self.lines, size)
+    def caps(self, word):
+        if word.isupper() and word != 'I':
+            word = word.lower()
+        elif word[0].isupper():
+            word = word.lower().capitalize()
+        else:
+            word = word.lower()
+        return word
 
-    def file_to_lines(self, fo):
-        lines = open(fo, 'r').readlines()
-        return lines
+    def file_to_words(self, filename):
+        with open(filename, 'r') as f:
+            words = [self.caps(w) for w in re.findall(r"[\w']+|[.,!?;]",
+                    f.read())]
+            return words
 
-    def _build_database(self, lines, size=3):
+    def temp_mapping(self, head, tail):
+        while len(head) > 0:
+            key = tuple(head)
+            if key in self.temp_db:
+                if tail in self.temp_db[key]:
+                    self.temp_db[key][tail] += 1.0
+                else:
+                    self.temp_db[key][tail] = 1.0
+            else:
+                self.temp_db[key] = {}
+                self.temp_db[key][tail] = 1.0
+            head = head[1:]
 
-        def ngrams(line, n=3):
-            if line < n:
-                return
-            return izip(*[line[i:] for i in range(n)])
+    def build_db(self, words, length=1):
+        self.starters.append(words[0])
+        for i in range(1, len(words) - 1):
+            if i <= length:
+                head = words[:i + 1]
+            else:
+                head = words[i - length + 1:i + 1]
+            tail = words[i + 1]
+            if head[-1] in '.' and tail not in '.,!?;':
+                self.starters.append(tail)
+            self.temp_mapping(head, tail)
+        for s, e in self.temp_db.iteritems():
+            total = sum(e.values())
+            self.db[s] = dict([(k, v / total) for k, v in e.iteritems()])
 
-        for line in lines:
-            line = line.split()
-            for a, b, c in ngrams(line, size):
-                state = (a, b)
-                rest = c
-                if state not in self.database:
-                    self.database[state] = {}
-                if rest not in self.database[state]:
-                    self.database[state] = [rest]
+    def next(self, previous):
+        sum = 0.0
+        r = ''
+        index = random.random()
+        while tuple(previous) not in self.db:
+            previous.pop(0)
+        for k, v in self.db[tuple(previous)].iteritems():
+            sum += v
+            if sum >= index and r == '':
+                r = k
+        return r
 
-    def generate(self, limit=None):
-        sentence = []
-        seed = choice(self.database.keys())
-        a, b = seed[0], seed[1]
-        while True:
-            if limit and len(sentence) >= limit:
-                break
-            try:
-                c = choice(self.database[(a, b)])
-            except KeyError:
-                break
-            sentence.append(c)
-            if c[-1] in Markov.EOS:
-                break
-            a, b = b, c
-        return ' '.join(sentence)
+    def gen(self, length=1):
+        current = random.choice(self.starters)
+        gave = current.capitalize()
+        previous = [current]
+        while current not in '.':
+            current = self.next(previous)
+            previous.append(current)
+            if len(previous) > length:
+                previous.pop(0)
+            if current not in '.,!?;':
+                gave += ' '
+            gave += current
+        return gave
+
 
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) < 2 or any([x for x in sys.argv if x in \
-            ('-h', '--help')]):
-        print('Usage:')
-        print('  {} <training> [n]'.format(__file__))
-        sys.exit(0)
-
     m = Markov(sys.argv[1])
-    n = int(sys.argv[2]) if len(sys.argv) > 2 else 1
-    for _ in range(n):
-        print m.generate()
+    print m.gen()
